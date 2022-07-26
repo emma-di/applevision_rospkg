1bb66f9d6c20806d95bc559ab957680f3b2f20ce
Grimmlins <osu.grimmlins@gmail.com>
2022-07-26 21:48:32 +0000

    attempting trigger variables + more attempts to get looping working

--- bin/dontdie.py
+++ bin/dontdie.py
@@ -10,26 +10,27 @@
 # To (hopefully) run this:
     # src/applevision_rospkg/bin/dontdie.py
 
-from applevision_motion import MotionPlanner, AppleApproach, MoveGroupInterface
-from enum import Enum#, auto
+from applevision_motion import MotionPlanner, AppleApproach
+#from enum import Enum#, auto
 from itertools import count
 def auto(it=count()):
     return it.next()
-from threading import Lock
-import traceback
+#from threading import Lock
+#import traceback
 #import sys
 
 import rospy
-import actionlib
-from actionlib import GoalStatus
+#import actionlib
+#from actionlib import GoalStatus
 from message_filters import Subscriber
-from moveit_msgs.msg import MoveGroupAction, MoveGroupGoal, Constraints, JointConstraint, PositionConstraint, BoundingVolume, OrientationConstraint
-from geometry_msgs.msg import PoseStamped
-from shape_msgs.msg import SolidPrimitive
+#from moveit_msgs.msg import MoveGroupAction, MoveGroupGoal, Constraints, JointConstraint, PositionConstraint, BoundingVolume, OrientationConstraint
+#from geometry_msgs.msg import PoseStamped
+#from shape_msgs.msg import SolidPrimitive
 from sensor_msgs.msg import Range
-from tf.listener import TransformListener
+from std_msgs.msg import Bool
+#from tf.listener import TransformListener
 
-from applevision_rospkg.srv import Tf2TransformPoseStamped
+#from applevision_rospkg.srv import Tf2TransformPoseStamped
 from applevision_rospkg.msg import RegionOfInterestWithConfidenceStamped, PointWithCovarianceStamped
 from helpers import RobustServiceProxy, ServiceProxyFailed, SynchronizerMinTick
 
@@ -45,8 +46,6 @@ Logger.setLevel(logging.DEBUG)
 rospy.init_node('applevision_motion')
 
 # setup for frame transformations (getting coords)
-#R = rospy.Rate(150)
-#broadcaster = tf.TransformBroadcaster()
 listener = tf.TransformListener()
 
 # initial joint positions
@@ -55,18 +54,12 @@ initial = [0, -.58, -2.26, -.44, 1.62, .74]
 # apple coords
 apple = [-.51, -.16, 1.3]
 
+# setup for the apple approach and motion (from applevision_motion)
 bot = MotionPlanner()
 planner = MotionPlanner()
 approach = AppleApproach(planner)
-
-GROUP_NAME = 'manipulator'
-EE_FRAME = 'palm'
-WORLD_FRAME = 'world'
-PLANNING_TIME = 5.0
 SYNC_SLOP = 0.2
 SYNC_TICK = 0.5
-MOVE_TOLERANCE = 0.03
-
 camera = Subscriber('applevision/apple_camera', RegionOfInterestWithConfidenceStamped, queue_size=10)
 dist = Subscriber('applevision/apple_dist', Range, queue_size=10)
 kal = Subscriber('applevision/est_apple_pos', PointWithCovarianceStamped, queue_size=10)
@@ -76,22 +69,37 @@ kal = Subscriber('applevision/est_apple_pos', PointWithCovarianceStamped, queue_
 # trials = []
 # results = []
 
+trigger = False
+
+# movement but with topics
+def move_to_home():
+    bot.moveToJointPosition(joints, initial)
+
+def apple_approach():
+    global trigger
+    min_tick = SynchronizerMinTick(
+        [kal, camera, dist], queue_size=20, slop=SYNC_SLOP, min_tick=SYNC_TICK)
+    min_tick.registerCallback(approach.tick_callback)
+    while True:
+        status = approach.planner.move_group_action.get_state()
+        Logger.debug(str(status))
+        if status in [3,2,4]:
+            trigger = True
+            break
+
 # loops through given number of times
 for x in range(int(input("Run how many times? "))):
     # reset
     result = "fail"
 
     # go to home position
-    bot.moveToJointPosition(joints, initial)
-    #print("move joints done")
+    move_to_home()
     
-    # the actual apple approach
-    min_tick = SynchronizerMinTick(
-        [kal, camera, dist], queue_size=20, slop=SYNC_SLOP, min_tick=SYNC_TICK)
-    min_tick.registerCallback(approach.tick_callback)
+    # the actual apple approach (from applevision_motion)
+    apple_approach()
     
     print("START SLEEPING")
-    rospy.sleep(120)
+    #rospy.sleep(120)
     print("LOOK HERE")
     # bot = applevision_motion.MoveGroupInterface()
     # bot.moveToJointPosition(joints, initial)
