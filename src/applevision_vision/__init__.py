4b5cdf75ee32cf26d0f076cf7c8a6f9ea5885813
cravetzm <emma.tingyu@gmail.com>
2023-07-07 18:23:56 +0000

    trying to implement yolov8. added old onnx file, will update once retrained

--- src/applevision_vision/__init__.py
+++ src/applevision_vision/__init__.py
@@ -1,4 +1,4 @@
 from pathlib import Path as _Path
 
 _self_path = _Path(__file__)
-MODEL_PATH = _self_path.parent.joinpath('best.onnx')
+MODEL_PATH = _self_path.parent.joinpath('best-roboflow.onnx')
